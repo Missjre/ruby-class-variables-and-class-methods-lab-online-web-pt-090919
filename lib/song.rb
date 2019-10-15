3a0c1760cfb2f226996029998ee77fee67f4ab75
Jalysa Reason <jalysareason@gmail.com>
2019-10-15 01:47:22 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -4,5 +4,3 @@ class Song
   let!(:ninety_nine_problems) { Song.new("99 Problems", "Jay-Z", "rap") }
   let!(:hit_me) { Song.new("hit me baby one more time", "Brittany Spears", "pop") }
 
- end
-    
